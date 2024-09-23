1adc3827827fd4e16498bfa828bb97c5e96574ea
Jakub <xjudiny@fi.muni.cz>
2024-09-23 14:37:41 +0200

    Add end-to-end test for site_a and site_b (check value presence)

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -5,3 +5,13 @@ test("find-watman", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByAltText("This is watman")).toBeInViewport();
 });
+
+test("find-factorial-value", async ({ page }) => {
+  await page.goto("/site_a.html");
+  await expect(page.getByText("120")).toBeInViewport();
+});
+
+test("find-fibonacci-value", async ({ page }) => {
+  await page.goto("/site_b.html");
+  await expect(page.getByText("5")).toBeInViewport();
+});
