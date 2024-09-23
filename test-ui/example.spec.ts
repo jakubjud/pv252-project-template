78fcb7914e7ea87319e710cf8074ba092113c115
Jakub <xjudiny@fi.muni.cz>
2024-09-23 23:43:53 +0200

    Add end-to-end test for menu links (and Hello World notification)

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -1,17 +1,31 @@
 import { expect } from "@playwright/test";
 import { test } from "./coverage_wrapper";
 
-test("find-watman", async ({ page }) => {  
+test("check-home-site", async ({ page }) => {
   await page.goto("/");
+  await expect(page.getByText("Hello World")).toBeInViewport();
   await expect(page.getByAltText("This is watman")).toBeInViewport();
+
+  await page.getByRole("link", { name: "Site A" }).click();
+  await expect(page).toHaveURL("/site_a.html");
+  await page.goto("/");
+  await page.getByRole("link", { name: "Site B" }).click();
+  await expect(page).toHaveURL("/site_b.html");
 });
 
-test("find-factorial-value", async ({ page }) => {
+test("check-factorial-site", async ({ page }) => {
   await page.goto("/site_a.html");
   await expect(page.getByText("120")).toBeInViewport();
+  await expect(page.getByText("5!")).toBeInViewport();
+
+  await page.getByRole("link", { name: "Home" }).click();
+  await expect(page).toHaveURL("/");
 });
 
-test("find-fibonacci-value", async ({ page }) => {
+test("check-fibonacci-site", async ({ page }) => {
   await page.goto("/site_b.html");
   await expect(page.getByText("5")).toBeInViewport();
+
+  await page.getByRole("link", { name: "Home" }).click();
+  await expect(page).toHaveURL("/");
 });
