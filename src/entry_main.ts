407e57800349aceffe6290d5de8560de2faecc53
Jakub <xjudiny@fi.muni.cz>
2024-12-02 13:42:19 +0100

    Feat: implement drawing using a web socket

--- src/entry_main.ts
+++ src/entry_main.ts
@@ -18,6 +18,7 @@ interface Point {
   y: number,
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface WelcomeMessage {
   // The size of the remote canvas.
   x: number,
@@ -33,22 +34,48 @@ interface UpdateMessage {
 // Create a websocket connection. 
 // More info at https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API
 const socket = new WebSocket("ws:socket.zavazadlo.unsigned-short.com");
-socket.onmessage = (m) => {
-  console.log(m)
-}
-
-// Example of how to use the canvas element:
-let canvas = new SocketCanvasElement();
-canvas.width = 128;
-canvas.height = 128;
+const canvas = new SocketCanvasElement();
 canvas.ondraw = (x,y) => {
-  console.log(x,y);
+  socket.send(JSON.stringify({
+    point: {"x": x, "y": y},
+    value: true
+  }));
+  canvas.setPixel(x, y, true);
 }
-document.querySelector("#container")!.appendChild(canvas);
 
-// We can only draw into canvas once it is actually shown, hence we postpose the draw operation.
-setTimeout(() => {
-  for (let x=0; x<128; x++) {
-    canvas.setPixel(x,x,true);
+// Poll for new updates
+socket.onopen = () => {
+  setInterval(() => {
+    setTimeout(() => {
+      socket.send(JSON.stringify({
+        point: {"x": 0, "y": 0},
+        value: false
+      }));
+    });
+  }, 500);
+}
+
+socket.onmessage = (m) => {
+  const data = JSON.parse(m.data);
+  if (canvas.width === null) {
+    canvas.width = data.x;
+    canvas.height = data.y;
+    document.querySelector("#container")!.appendChild(canvas);
+
+    // We can only draw into canvas once it is actually shown, hence we postpose the draw operation.
+    setTimeout(() => {
+      for (let x = 0; x < data.x; x++) {
+        for (let y = 0; y < data.y; y++) {
+          if (data.data[x * data.x + y] === 1) {
+            canvas.setPixel(x, y, true);
+          }
+        }
+      }
+    })
   }
-})
+  else {
+    data.forEach((update: UpdateMessage) => {
+      canvas.setPixel(update.point.x, update.point.y, update.value);
+    })
+  }
+}
